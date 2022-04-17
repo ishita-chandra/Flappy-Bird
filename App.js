56f22225f1e1f195bc10112f998085f5435507f5
ishita-chandra <ishitachandra2001@gmail.com>
2022-04-18 03:59:07 +0530

    initialised files of flappy bird

--- App.js
+++ App.js
@@ -1,20 +1,198 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, TouchableOpacity, View, Button } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
+import entities from './entities';
+import Physics from './physics';
+import { GoogleAuthProvider, signInWithPopup, } from 'firebase/auth'
+import { auth, db } from './firebase'
+import { doc, setDoc, collection, getDoc } from 'firebase/firestore'
+import '@expo/match-media'
+import { useMediaQuery } from "react-responsive";
+
+
 
 export default function App() {
+
+  const [running, setRunning] = useState(false)
+  const [gameEngine, setGameEngine] = useState(null)
+  const [currentPoints, setCurrentPoints] = useState(0)
+  const [highest, setHighest] = useState(0)
+  const [user, setUser] = useState(null)
+  const fetchGHighest = async () => {
+    const docref = doc(db, "GlobalHighest", "public")
+    try {
+      const result = await getDoc(docref)
+      // console.log(result.data())
+      // const m=result.data()["globalmax"]
+      const { globalmax } = result.data();
+      setHighest(globalmax)
+    }
+    catch (err) {
+      console.log(err)
+    }
+
+  }
+  useEffect(() => {
+    fetchGHighest();
+    setRunning(false)
+
+
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const docref = doc(db, "users", user.uid)
+        try {
+          const result = await getDoc(docref)
+          setUser(result.data())
+        }
+        catch (err) {
+          console.log(err)
+        }
+      }
+    })
+  }, [])
+
+useEffect(()=>{
+  if (!running && auth.currentUser !== null && currentPoints > user.maxScore ) {
+    updateMaxScore();
+  }
+
+},[running])
+
+
+
+
+
+
+
+
+  const login = async () => {
+
+    const provider = new GoogleAuthProvider()
+    try {
+      const result = await signInWithPopup(auth, provider)
+
+      const docref = doc(db, "users", result.user.uid)
+      const data = {
+        maxScore: 0,
+        username: result.user.email.split('@')[0]
+      }
+      const res = await setDoc(docref, data, {
+        merge: true,
+      })
+      setUser(data)
+    }
+    catch (err) {
+      console.log(err);
+      alert("Error signing in")
+    }
+  }
+  const signOut = () => {
+    auth.signOut();
+  }
+  const updateGHighest = async () => {
+    try {
+      const docref = doc(db, "GlobalHighest", "public")
+      const data = {
+        globalmax: currentPoints,
+      }
+      const res = await setDoc(docref, data, {
+        merge: true,
+      })
+      setHighest(currentPoints)
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  //onsole.log(user)
+
+  const updateMaxScore = async () => {
+  console.log("entered")
+
+    try {
+      const docref = doc(db, "users", auth.currentUser.uid)
+      const data = {
+        maxScore: currentPoints,
+        
+      }
+      
+      const res = await setDoc(docref, data, {
+        merge: true,
+      })
+      setUser({ ...user, maxScore: currentPoints })
+      currentPoints>highest && updateGHighest()
+    }
+    catch (e) {
+      console.log(e)
+      alert("Error updating high score")
+    }
+  }
+
+  const isTabletOrMobileDevice = useMediaQuery({    
+    maxDeviceWidth: 1000,
+    // alternatively...
+    query: "(max-device-width: 1000px)"  
+  });
+  if (!isTabletOrMobileDevice) {
+    return (<View style={{flexGrow:1, alignItems: 'center',justifyContent: 'center',padding:20}}>
+       <Text style={{ fontWeight: 900,fontSize:30}}>Hi Desktop Users 👋, Please switch to mobile or tablet to play this game!</Text></View>)
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={{ flex: 1, alignItems: 'center',objectFit:"cover",backgroundImage:"url(https://images.unsplash.com/photo-1517210122415-b0c70b2a09bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2xvdWQlMjBhZXN0aGV0aWN8ZW58MHx8MHx8&w=1000&q=80)" }}>
+      <Text style={{ textAlign: 'center', fontSize: 40, fontWeight: 'bold', margin: 20 , zIndex:150}}>{currentPoints}</Text>
+      {!running && <Text onPress={login} style={{ zIndex: 200, fontWeight: 'bold', color: 'white', fontSize: 30, cursor: 'pointer', backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10, textAlign: 'center' }}>
+        {auth.currentUser !== null ? `Welcome, ${auth.currentUser.displayName}!` : "Login"}
+      </Text>}
+      {auth.currentUser !== null && !running && <Text onPress={signOut} style={{ position: 'absolute', top: 0, right: 0, zIndex: 200, fontWeight: 'bold', color: 'white', fontSize: 30, cursor: 'pointer', backgroundColor: 'black' }}>
+        Sign Out
+      </Text>}
+      {auth.currentUser !== null && <Text onPress={signOut} style={{ position: 'absolute', bottom: 0, zIndex: 200, fontWeight: 'bold', color: 'white', fontSize: 30, cursor: 'pointer', backgroundColor: 'black' }}>
+        Your Highest Score : {user?.maxScore}<br />
+        //Global Highest Score: {highest}
+      </Text>}
+      { !running  && <Text onPress={signOut} style={{ position: 'absolute', bottom: 0, zIndex: 200, fontWeight: 'bold', color: 'white', fontSize: 30, cursor: 'pointer', backgroundColor: 'black' }}>
+        {/*Your Highest Score : {user?.maxScore}<br /> */}
+        Global Highest Score: {highest}
+      </Text>}
+      <GameEngine
+        ref={(ref) => { setGameEngine(ref) }}
+        systems={[Physics]}
+        entities={entities()}
+        running={running}
+        onEvent={(e) => {
+          switch (e.type) {
+            case 'game_over':
+              setRunning(false)
+              gameEngine.stop()
+              break;
+            case 'new_point':
+              setCurrentPoints(currentPoints + 1)
+
+              break;
+          }
+        }}
+        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+      >
+        <StatusBar style="auto" hidden={true} />
+
+      </GameEngine>
+
+      {!running ?
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <TouchableOpacity style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10 }}
+            onPress={() => {
+              setCurrentPoints(0)
+              setRunning(true)
+              gameEngine.swap(entities())
+            }}>
+            <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
+              START GAME
+            </Text>
+          </TouchableOpacity>
+
+        </View> : null}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Bird.js
@@ -0,0 +1,55 @@
+import Matter from 'matter-js'
+import React from 'react'
+import { View, Image } from 'react-native'
+
+
+const Bird = props => {
+    const widthBody = props.body.bounds.max.x - props.body.bounds.min.x
+    const heightBody = props.body.bounds.max.y - props.body.bounds.min.y
+
+    const xBody = props.body.position.x - widthBody /2
+    const yBody = props.body.position.y - heightBody /2
+
+    const color = props.color;
+
+    return(
+
+        
+        <View style={{
+           // borderWidth: 1,
+           borderRadius: 5,
+            //backgroundSize:"contain",
+           // backgroundRepeat: "no-repeat",
+           // backgroundImage: "url(https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Tweety.svg/800px-Tweety.svg.png)",
+           // borderStyle: 'solid',
+            position:'relative',
+            left: xBody,
+            top: yBody,
+            width: widthBody,
+            height: heightBody
+        }}>
+            <Image style={{position:'absolute',inset:0,resizeMode:'scale'}}
+            
+            
+            source={{uri:'https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Tweety.svg/800px-Tweety.svg.png'}}/>
+        </View>
+    )
+}
+
+export default (world, color, pos, size) => {
+   const initialBird = Matter.Bodies.rectangle(
+       pos.x,
+       pos.y,
+       size.width,
+       size.height,
+       {label: 'Bird'}
+   )
+   Matter.World.add(world, initialBird)
+
+   return {
+       body: initialBird,
+       color,
+       pos,
+       renderer: <Bird/>
+   }
+}
